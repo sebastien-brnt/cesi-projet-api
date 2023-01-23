3eed6aa4432ef3ba61291e8927d94181d222a538
sebastien-brnt <90051256+sebastien-brnt@users.noreply.github.com>
2023-01-23 14:41:28 +0100

    Mise en place route api contact

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
-const  express = require("express");
+const express = require("express");
 const app = express();
 // app.use(bodyParser.json());
+
+const contacts = [
+  {
+    id: 1,
+    prenom: "Sébastien",
+    nom: "Burnet",
+    telephone: ["0234545456"],
+  },
+  {
+    id: 2,
+    prenom: "Youenn",
+    nom: "Rialet",
+    telephone: ["0234545456"],
+  },
+  {
+    id: 3,
+    nom: "Vermonden",
+    prenom: "Daniel",
+    telephones: ["0454565433"],
+  },
+  {
+    id: 4,
+    nom: "Dubois",
+    prenom: "Jean",
+    telephones: ["0234545456"],
+  },
+];
+
 app.get("/", function (req, res) {
   res.send("Hello EPSI");
 });
+
+app.get("/api/contacts", function (req, res) {
+  // res.send(contacts);
+  res.status(200).json(contacts);
+});
+
 app.listen(3002);
