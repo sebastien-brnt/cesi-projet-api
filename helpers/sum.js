526eb191830ad5928c36c8a9137351fd114ef9b0
sebastien-brnt <90051256+sebastien-brnt@users.noreply.github.com>
2023-01-23 14:41:45 +0100

    Mise en place fonction sum()

---
+++ helpers/sum.js
@@ -0,0 +1,9 @@
+function sum(number1, number2) {
+  if (Number.isInteger(number1) && Number.isInteger(number2)) {
+    return number1 + number2;
+  }
+
+  return "Erreur : Une des valeurs renseignées n'est pas un nombre";
+}
+
+module.export = sum;
